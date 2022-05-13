564984e451f5528b1d5cef4a8cdecca31acdf7aa
dani <danifullstack@gmail.com>
2022-05-13 11:50:29 -0700

    added csp for some scripts

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ dotenv.config();
 import Stripe from "stripe";
 import fs from "fs";
 import { promisify } from "util";
-import http2 from 'http2';
+import http2 from "http2";
 const stripe = new Stripe(process.env.STRIPE_SECRET);
 
 const __filename = fileURLToPath(import.meta.url);
@@ -31,30 +31,34 @@ const coinpaymentsClient = new Coinpayments({
   secret: process.env.COIN_PAYMENTS_PRIVATE,
 });
 
-
-
 app.use(compression());
 app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptScr: [
+        "'self'",
+        "https://unpkg.com/react/umd/react.production.min.js",
+        "https://unpkg.com/react-dom/umd/react-dom.production.min.js",
+        "https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js",
+      ],
+    },
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 if (!(process.env.NODE_ENV === "production")) {
   // Handle app on development
   app.use(cors());
-}else{
-  app.use((req,res,next) => {
-    // Handle app in production
-    // res.set('Access-Control-Allow-Origin: http://js.stripe.com/');
-    // res.set("Content-Security-Policy", "script-src 'self' https://stripe.com");
-    next();
-  })
 }
 app.use("/", router);
 if (process.env.NODE_ENV === "production") {
   app.use(expressStaticGzip(path.join(__dirname, "..", "client", "build")));
   app.get("*", async (_, res) => {
-      res.sendFile(
-        path.resolve(__dirname, "..", "client", "build", "index.html")
-      );
+    res.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
   });
 }
 const PORT = process.env.PORT || 5000;
@@ -68,8 +72,8 @@ mongoose
         key: fs.readFileSync("./cert/server.key"),
         cert: fs.readFileSync("./cert/server.crt"),
         spdy: {
-          protocols: ['h2']
-        }
+          protocols: ["h2"],
+        },
       },
       app
     );
