d62d0ccb72054c2469e9c1a87eb49ff7d2a02c22
dani <danifullstack@gmail.com>
2022-05-26 02:57:02 -0700

    removed csp momentarily

--- server/server.js
+++ server/server.js
@@ -30,16 +30,17 @@ const coinpaymentsClient = new Coinpayments({
 });
 
 app.use(compression());
-app.use(helmet({ contentSecurityPolicy: false }));
-// manually set up csp since helmet's csp doesn't seem to work properly
-app.use((req, res, next) => {
-  res.set(
-    "Content-Security-Policy",
-    "default-src 'self'; script-src 'self' https://unpkg.com/react/umd/react.production.min.js https://unpkg.com/react-dom/umd/react-dom.production.min.js https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js https://connect.facebook.net/en_US/sdk.js https://js.stripe.com/v3 www.facebook.com; style-src 'self' https://connect.facebook.net/en_US/sdk.js;"
-  );
-  next();
+// app.use(helmet({ contentSecurityPolicy: false }));
+// // manually set up csp since helmet's csp doesn't seem to work properly
+// app.use((req, res, next) => {
+//   res.set(
+//     "Content-Security-Policy",
+//     "default-src 'self'; script-src 'self' https://unpkg.com/react/umd/react.production.min.js https://unpkg.com/react-dom/umd/react-dom.production.min.js https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js https://connect.facebook.net/en_US/sdk.js https://js.stripe.com/v3 www.facebook.com; style-src 'self' https://connect.facebook.net/en_US/sdk.js;"
+//   );
+//   next();
 
-});
+// });
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", router);
